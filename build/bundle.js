ef9f4865bf2365ad6766dc1415bf46e9f9f50082
ankihg <hagelin.annika@gmail.com>
2016-07-04 15:00:31 -0700

    toys-ad for side-bar

--- build/bundle.js
+++ build/bundle.js
@@ -65251,11 +65251,23 @@
 	    return {
 	      restrict: 'E',
 	      repalce: true,
-	      templateUrl: './directives/templates/side-bar.html',
+	      templateUrl: './directives/templates/side-bar/index.html',
 	      controller: 'NavController',
 	      controllerAs: 'navCtrl'
 	    }
 	  });
+
+	  app.directive('toysAd', () => {
+	    return {
+	      restrict: 'E',
+	      replace: true,
+	      templateUrl: './directives/templates/side-bar/toys-ad.html',
+	      controller: 'ToysController',
+	      controllerAs: 'toysCtrl'
+	    }
+	  })
+
+
 	}
 
 
--- src/directives/side-bar.directive.js
+++ src/directives/side-bar.directive.js
@@ -3,9 +3,21 @@ module.exports = (app) => {
     return {
       restrict: 'E',
       repalce: true,
-      templateUrl: './directives/templates/side-bar.html',
+      templateUrl: './directives/templates/side-bar/index.html',
       controller: 'NavController',
       controllerAs: 'navCtrl'
     }
   });
+
+  app.directive('toysAd', () => {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: './directives/templates/side-bar/toys-ad.html',
+      controller: 'ToysController',
+      controllerAs: 'toysCtrl'
+    }
+  })
+
+
 }
