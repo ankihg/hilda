083d95cc5912465c5d5bc4268ba90fa1e894dfbe
ankihg <hagelin.annika@gmail.com>
2016-07-18 11:42:19 -0700

    goodbye directives

--- src/directives/nav-button.directive.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  app.directive('navButton', () => {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: './directives/templates/nav-button.html'
-    }
-  })
-}
--- src/directives/toy.directive.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  app.directive('toy', () => {
-    return {
-      restrict: 'E',
-      repalce: true,
-      templateUrl: './directives/templates/toy.html'
-    }
-  });
-}
