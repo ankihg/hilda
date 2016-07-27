ccc8083df20ff8917f471673770d39c10e086a03
ankihg <hagelin.annika@gmail.com>
2016-07-27 13:43:30 -0700

    nav to foreign host

--- build/bundle.js
+++ build/bundle.js
@@ -64966,20 +64966,21 @@
 
 	    this.plz = "h i l d a";
 
-	    var Link = function(name, path, pos) {
+	    var Link = function(name, path, pos, foreign) {
 	      this.name = name;
 	      this.path = path;
 	      this.pos = pos;
+	      this.foreign = foreign;
 	    }
 	    Link.prototype.go = function() {
-	      $location.path(this.path);
+	      (!this.foreign) ? $location.path(this.path) : window.open(this.path, '_blank');
 	    }
 
 	    this.links = {
 	      home: new Link('home', '/', {x: 8, y: 20}),
 	      toys: new Link('toys', '/toys', {x: 12, y: 40}),
 	      bio: new Link('bio', '/bio', {x: 8, y: 60}),
-	      flickr: new Link('flickr', '/flickr', {x: 86, y: 20}),
+	      flickr: new Link('flickr', 'https://www.flickr.com/photos/142887762@N06/', {x: 86, y: 20}, true),
 	      squirrels: new Link('squirrels', '/squirrel-report', {x: 82, y: 40}),
 	      plz: new Link('plz', '/plz', {x: 86, y: 60})
 	    }
