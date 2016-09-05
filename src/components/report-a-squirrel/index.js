77c15aa158051afdedabb491033aee006b16e808
ankihg <hagelin.annika@gmail.com>
2016-09-05 13:18:02 -0700

    submit w or w/o geolocate

--- src/components/report-a-squirrel/index.js
+++ src/components/report-a-squirrel/index.js
@@ -41,15 +41,32 @@ module.exports = function(app) {
       }
 
       vm.submit = function() {
-        console.log('geolocate');
-        console.log(vm.geolocate);
-        console.log('get location');
-        if (navigator.geolocation) {
+        if (!vm.reporter.newSighting.reporter)
+          alert('who is reporting this squirrel sighting?');
+
+        if (vm.geolocate && navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             console.log('here is ur location');
             console.log(position);
+            vm.reporter.newSighting.loc.latitude = position.coords.latitude;
+            vm.reporter.newSighting.loc.longitude = position.coords.longitude;
+            console.log('report it w geoloc');
+            vm.reporter.reportSighting();
           });
+        } else {
+          console.log('report w/o geoloc');
+          if (vm.reporter.newSighting.loc.latitude && vm.reporter.newSighting.loc.longitude)
+            vm.reporter.reportSighting();
+          else
+            alert('plz input lat & lng of squirrel sighting')
         }
+
+        // if (navigator.geolocation) {
+        //   navigator.geolocation.getCurrentPosition(function(position) {
+        //     console.log('here is ur location');
+        //     console.log(position);
+        //   });
+        // }
         // vm.reporter.reportSighting();
       }
 
